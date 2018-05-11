7cbbfd80ced0af5006269ace7337b54a22bd88fc
Drew Martin <drew.martin.developer@gmail.com>
2018-05-10 22:22:15 -0400

    Removed extra interface files. Combined IReadOnly- files into the regular "writeable" files.

--- Framework/Builders/IBuilder.cs
+++ Framework/Builders/IBuilder.cs
@@ -1,8 +1,23 @@
-﻿using System;
+﻿using Atlas.Framework.Signals;
+using System;
 
 namespace Atlas.Framework.Builders
 {
-	interface IBuilder<T> : IReadOnlyBuilder<T>
+	public interface IReadOnlyBuilder<T>
+	{
+		/// <summary>
+		/// The current state of the build process. This can be
+		/// unbuilt, building, or built.
+		/// </summary>
+		BuildState BuildState { get; }
+
+		/// <summary>
+		/// Signal dispatching when the BuildState has changed.
+		/// </summary>
+		ISignal<T, BuildState, BuildState> BuildStateChanged { get; }
+	}
+
+	public interface IBuilder<T> : IReadOnlyBuilder<T>
 	{
 		/// <summary>
 		/// The current state of the build process. This can be
--- Framework/Collections/EngineList/IEngineList.cs
+++ Framework/Collections/EngineList/IEngineList.cs
@@ -2,6 +2,13 @@
 
 namespace Atlas.Framework.Collections.EngineList
 {
+	public interface IReadOnlyEngineList<T> : IReadOnlyList<T>
+	{
+		bool Contains(T item);
+		IEnumerable<T> Forward();
+		IEnumerable<T> Backward();
+	}
+
 	public interface IEngineList<T> : IReadOnlyEngineList<T>, IList<T>
 	{
 
