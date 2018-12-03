4f9185bf6bb6b5f5229b09fb4d8d6944186c0b3a
Drew Martin <drew.martin.developer@gmail.com>
2018-12-02 19:00:46 -0500

    Added ease of use methods to ITransform2D to only change the X or Y values of things, or set them all in bulk to reduce excessive Matrix recalculations.

--- Framework/Components/Transform/Transform2D/ITransform2D.cs
+++ Framework/Components/Transform/Transform2D/ITransform2D.cs
@@ -8,5 +8,13 @@ namespace Atlas.Framework.Components.Transform
 		Vector2 Position { get; set; }
 		Vector2 Scale { get; set; }
 		float Rotation { get; set; }
+
+		void Set(Vector2 position, float rotation, Vector2 scale);
+
+		float PositionX { get; set; }
+		float PositionY { get; set; }
+		float ScaleX { get; set; }
+		float ScaleY { get; set; }
+		float ScaleXY { get; set; }
 	}
 }
--- Framework/Components/Transform/Transform2D/Transform2D.cs
+++ Framework/Components/Transform/Transform2D/Transform2D.cs
@@ -1,12 +1,15 @@
 ﻿using Atlas.Core.Utilites;
 using Atlas.ECS.Components;
 using Atlas.ECS.Entities;
+using Atlas.ECS.Entities.Messages;
 using Microsoft.Xna.Framework;
 
 namespace Atlas.Framework.Components.Transform
 {
 	public class Transform2D : AtlasComponent, ITransform2D
 	{
+		private bool recalculate = true;
+
 		private Vector2 position;
 		private Vector2 scale;
 		private float rotation;
@@ -17,18 +20,25 @@ namespace Atlas.Framework.Components.Transform
 
 		public Transform2D(Vector2 position, float rotation, Vector2 scale)
 		{
-			Position = position;
-			Rotation = rotation;
-			Scale = scale;
+			Set(position, rotation, scale);
 		}
 
 		protected override void AddingManager(IEntity entity, int index)
 		{
 			base.AddingManager(entity, index);
+			entity.AddListener<IGlobalMatrixMessage>(SetMatrix);
 			SetMatrix();
 		}
 
-		public virtual Vector2 Position
+		protected override void RemovingManager(IEntity entity, int index)
+		{
+			entity.RemoveListener<IGlobalMatrixMessage>(SetMatrix);
+			base.RemovingManager(entity, index);
+		}
+
+		#region Position
+
+		public Vector2 Position
 		{
 			get { return position; }
 			set
@@ -36,11 +46,28 @@ namespace Atlas.Framework.Components.Transform
 				if(position == value)
 					return;
 				position = value;
+				//Send event
 				SetMatrix();
 			}
 		}
 
-		public virtual Vector2 Scale
+		public float PositionX
+		{
+			get { return position.X; }
+			set { Position = new Vector2(value, position.Y); }
+		}
+
+		public float PositionY
+		{
+			get { return position.Y; }
+			set { Position = new Vector2(position.X, value); }
+		}
+
+		#endregion
+
+		#region Scale
+
+		public Vector2 Scale
 		{
 			get { return scale; }
 			set
@@ -48,11 +75,34 @@ namespace Atlas.Framework.Components.Transform
 				if(scale == value)
 					return;
 				scale = value;
+				//Send event
 				SetMatrix();
 			}
 		}
 
-		public virtual float Rotation
+		public float ScaleX
+		{
+			get { return scale.X; }
+			set { Scale = new Vector2(value, scale.Y); }
+		}
+
+		public float ScaleY
+		{
+			get { return scale.Y; }
+			set { Scale = new Vector2(scale.X, value); }
+		}
+
+		public float ScaleXY
+		{
+			get { return (scale.X * scale.Y) / 2; }
+			set { Scale = new Vector2(value, value); }
+		}
+
+		#endregion
+
+		#region Rotation
+
+		public float Rotation
 		{
 			get { return rotation; }
 			set
@@ -61,12 +111,52 @@ namespace Atlas.Framework.Components.Transform
 				if(rotation == value)
 					return;
 				rotation = value;
+				//Send event
 				SetMatrix();
 			}
 		}
 
+		#endregion
+
+		/// <summary>
+		/// Set all Transform values in bulk before applying changes to the Entity's LocalMatrix.
+		/// Reduces the amount of recalculations and messages that get sent.
+		/// </summary>
+		/// <param name="position"></param>
+		/// <param name="rotation"></param>
+		/// <param name="scale"></param>
+		public void Set(Vector2 position, float rotation, Vector2 scale)
+		{
+			Recalculate = false;
+			Position = position;
+			Rotation = rotation;
+			Scale = scale;
+			Recalculate = true;
+		}
+
+		protected bool Recalculate
+		{
+			get { return recalculate; }
+			set
+			{
+				if(recalculate == value)
+					return;
+				recalculate = value;
+				if(recalculate)
+					SetMatrix();
+			}
+		}
+
+		private void SetMatrix(IGlobalMatrixMessage message)
+		{
+			if(message.Hierarchy)
+				SetMatrix();
+		}
+
 		protected void SetMatrix()
 		{
+			if(!recalculate)
+				return;
 			if(Manager != null)
 				Manager.LocalMatrix = CreateLocalMatrix();
 		}
