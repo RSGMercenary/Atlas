f82adc042c7bd72b9b9201c0b9e5fe2a3c015145
Drew Martin <drew.martin.developer@gmail.com>
2018-12-02 19:11:15 -0500

    Renamed EntityCreator to AtlasGame to better claify what it does. Removed unused/moved classes.

--- ECS/Components/Engine/AtlasTestEngine.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Atlas.ECS.Components
-{
-	public sealed class AtlasTestEngine : AtlasEngine
-	{
-	}
-}
--- Framework/AtlasGame.cs
+++ Framework/AtlasGame.cs
@@ -1,12 +1,16 @@
 ﻿using Atlas.ECS.Components;
 using Atlas.ECS.Entities;
+using Atlas.Framework.Components.Manager;
 using Atlas.Framework.Components.Render;
 using Atlas.Framework.Components.Transform;
+using Atlas.Framework.Systems.Render;
+using Atlas.Framework.Systems.Transform;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
-namespace Atlas.Framework.Entities
+namespace Atlas.Framework
 {
-	public static class EntityCreator
+	public static class AtlasGame
 	{
 		/// <summary>
 		/// <para>
@@ -19,22 +23,20 @@ namespace Atlas.Framework.Entities
 		/// 3. "Root"->"Back GUI" IEntity
 		/// 4. "Root"->"World" IEntity
 		/// 5. "Root"->"Front GUI" IEntity
+		/// 6. "Root"->"Cursor" Entity
 		/// </para>
 		/// </summary>
 		/// <returns></returns>
-		public static IEngine InitializeMonoGame(Game game)
+		public static IEngine Create(Game game)
 		{
-			//Initial Game instance setup.
-			game.Window.AllowUserResizing = true;
-			game.Content.RootDirectory = "Content";
-			//Atlas handles fixed timestep. Let MonoGame run as fast as possible.
-			game.IsFixedTimeStep = false;
-			game.IsMouseVisible = true;
-
 			var root = new AtlasEntity(true);
-			root.AddComponent<IEngine>(new AtlasTestEngine());
-			root.AddComponent<IRenderer2D>(new Renderer2D(game.GraphicsDevice));
+			root.AddComponent<IEngine>(new AtlasEngine());
+			root.AddComponent<IGameManager>(new GameManager(game));
 			root.AddComponent<ITransform2D>(new Transform2D());
+			root.AddComponent<ISystemManager>(new SystemManager(
+				typeof(Camera2DSystem),
+				typeof(Cursor2DSystem),
+				typeof(Render2DSystem)));
 
 			//Back GUI. Drawn under the world. (X, Y) coordinates are at (0, 0).
 			//Not sure what it could be used for, but... I set it up.
@@ -47,13 +49,19 @@ namespace Atlas.Framework.Entities
 			Transform scale determines how zoomed in the world is.
 			*/
 			var world = root.AddChild("World", true);
-			world.AddComponent<ITransform2D>(new CameraTransform2D(game));
+			world.AddComponent<ITransform2D>(new CameraTransform2D());
 			world.AddComponent<ICamera2D>(new Camera2D());
 
 			//Front GUI. Drawn over the world. (X, Y) coordinates are at (0, 0).
 			//Use this to add layers for HUDs, menus, popups, etc.
 			var front = root.AddChild("Front GUI", true);
 			front.AddComponent<ITransform2D>(new Transform2D());
+
+			//The cursor. Goes in front of all other object.
+			var cursor = root.AddChild("Cursor", true);
+			cursor.AddComponent<ITransform2D>(new CursorTransform2D());
+			cursor.AddComponent<ICursor2D>(new Cursor2D());
+			cursor.AddComponent<IRender2D>(new Render2D(new Texture2D(game.GraphicsDevice, 5, 5), Color.White));
 
 			return root.GetComponent<IEngine>();
 		}
