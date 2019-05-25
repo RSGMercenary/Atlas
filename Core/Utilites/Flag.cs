df61210383325419a0faf4ae851a4f42689267d9
Drew Martin <drew.martin.developer@gmail.com>
2019-05-25 17:00:59 -0400

    Added Enum utility to handle iterating over flag values.

---
+++ Core/Utilites/Flag.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atlas.Core.Utilites
+{
+	public static class Flag
+	{
+		public static IEnumerable<T> GetFlags<T>(this T item, bool zero = false)
+			where T : Enum
+		{
+			foreach(T value in Enum.GetValues(typeof(T)))
+			{
+				if(value.OneFlag(zero) && item.HasFlag(value))
+					yield return value;
+			}
+		}
+
+		public static T SetFlags<T>(this IEnumerable<T> items)
+			where T : Enum
+		{
+			int flags = 0;
+			foreach(var item in items)
+				flags |= Convert.ToInt32(item);
+			return (T)Enum.Parse(typeof(T), flags.ToString());
+		}
+
+		public static bool AnyFlags<T>(this T flag1, T flag2)
+			where T : Enum
+		{
+			foreach(T value in Enum.GetValues(typeof(T)))
+			{
+				if(flag1.HasFlag(value) && flag2.HasFlag(value))
+					return true;
+			}
+			return false;
+		}
+
+		public static bool OneFlag(this Enum item, bool zero = false)
+		{
+			int value = Convert.ToInt32(item);
+			return (value != 0 || zero) && ((value & (value - 1)) == 0);
+		}
+	}
+}
