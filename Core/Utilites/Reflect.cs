f11f58fb581e6f9057975f224257d8a0312c0a58
Drew Martin <drew.martin.developer@gmail.com>
2019-05-27 02:51:21 -0400

    Added Reflection extensions for Types to get all fields from all base classes.

---
+++ Core/Utilites/Reflect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Atlas.Core.Utilites
+{
+	public static class Reflect
+	{
+		public static IEnumerable<FieldInfo> GetAllFields(this Type type, BindingFlags flags)
+		{
+			if(type == null)
+				yield break;
+			foreach(var field in type.GetFields(flags | BindingFlags.DeclaredOnly))
+				yield return field;
+			foreach(var field in GetAllFields(type.BaseType, flags))
+				yield return field;
+		}
+	}
+}
