af73423955b57006de61580998672521aae2ff1d
Drew Martin <drew.martin.developer@gmail.com>
2019-11-09 17:50:45 -0500

    Added in keyword to all IDispatch interfaces.

--- Core/Signals/IDispatch.cs
+++ Core/Signals/IDispatch.cs
@@ -5,22 +5,22 @@
 		bool Dispatch();
 	}
 
-	public interface IDispatch<T1>
+	public interface IDispatch<in T1>
 	{
 		bool Dispatch(T1 item1);
 	}
 
-	public interface IDispatch<T1, T2>
+	public interface IDispatch<in T1, in T2>
 	{
 		bool Dispatch(T1 item1, T2 item2);
 	}
 
-	public interface IDispatch<T1, T2, T3>
+	public interface IDispatch<in T1, in T2, in T3>
 	{
 		bool Dispatch(T1 item1, T2 item2, T3 item3);
 	}
 
-	public interface IDispatch<T1, T2, T3, T4>
+	public interface IDispatch<in T1, in T2, in T3, in T4>
 	{
 		bool Dispatch(T1 item1, T2 item2, T3 item3, T4 item4);
 	}
