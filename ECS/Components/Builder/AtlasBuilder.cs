d0240757665fd8cf85556c20a2ba5c99e505400f
Drew Martin <drew.martin.developer@gmail.com>
2019-11-09 17:08:43 -0500

    Made field read-only.

--- ECS/Components/Builder/AtlasBuilder.cs
+++ ECS/Components/Builder/AtlasBuilder.cs
@@ -8,7 +8,7 @@ namespace Atlas.ECS.Components.Builder
 {
 	public abstract class AtlasBuilder : AtlasComponent<IBuilder>, IBuilder
 	{
-		private Stack<Action> builders = new Stack<Action>();
+		private readonly Stack<Action> builders = new Stack<Action>();
 		private BuildState state = BuildState.Unbuilt;
 		private bool autoRemove = true;
 
