0bc54d1a7314fd2abd60de3cced1f144ea3aa0cb
RSGMercenary <drew.martin.developer@gmail.com>
2024-08-14 19:56:52 -0400

    Fixed TypeExtensions bugs.

--- Atlas.ECS/Core/Extensions/TypeExtensions.cs
+++ Atlas.ECS/Core/Extensions/TypeExtensions.cs
@@ -5,21 +5,21 @@ namespace Atlas.Core.Extensions;
 
 public static class TypeExtensions
 {
-	public static Type GetInterfaceType<T>(this T instance) => GetInterfaceType<T>(instance.GetType());
+	public static Type GetInterfaceType<T>(this T instance, bool inclusive = false) => GetInterfaceType<T>(instance.GetType(), inclusive);
 
-	private static Type GetInterfaceType<T>(Type instanceType)
+	private static Type GetInterfaceType<T>(Type instanceType, bool inclusive = false)
 	{
 		var type = typeof(T);
 		var interfaces = instanceType.GetInterfaces()
 			.Except(instanceType.BaseType?.GetInterfaces() ?? Enumerable.Empty<Type>())
-			.Where(t => t != type && t.IsAssignableTo(type));
+			.Where(i => i == type == inclusive && i.IsAssignableTo(type));
 
 		if(interfaces.Skip(1).Any())
-			throw new Exception();
+			throw new ArgumentException();
 		if(interfaces.Any())
 			return interfaces.First();
 		if(instanceType.BaseType != null)
-			return GetInterfaceType(instanceType.BaseType);
+			return GetInterfaceType<T>(instanceType.BaseType, inclusive);
 
 		return null;
 	}
