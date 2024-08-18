2ac9cd8a6187575683be52bd771a7bf83738b7c6
RSGMercenary <drew.martin.developer@gmail.com>
2024-08-17 21:20:29 -0400

    Changed RandomExtendsions namespace.

---
+++ Atlas.Tests/Classes/RandomExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Atlas.Tests.Classes;
+
+static class RandomExtensions
+{
+	public static bool NextBool(this Random random) => random.NextDouble() >= 0.5;
+
+	public static int NextExclude(this Random random, int maxValue, int exclude)
+	{
+		var value = random.Next(maxValue);
+		while(value == exclude)
+			value = random.Next(maxValue);
+		return value;
+	}
+}
--- Atlas.Tests/ECS/Serialization/RandomExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Atlas.Tests.ECS.Serialization;
-
-static class RandomExtensions
-{
-	public static bool NextBool(this Random random) => random.NextDouble() >= 0.5;
-
-}
