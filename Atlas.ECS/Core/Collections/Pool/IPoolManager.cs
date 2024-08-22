4e8f25bf7a5e8240ea71f28aa1f817f3607a4bbf
RSGMercenary <drew.martin.developer@gmail.com>
2024-08-21 21:29:55 -0400

    Added new PoolManager.

---
+++ Atlas.ECS/Core/Collections/Pool/IPoolManager.cs
@@ -0,0 +1,24 @@
+﻿using Atlas.Core.Messages;
+using System;
+using System.Collections.Generic;
+
+namespace Atlas.Core.Collections.Pool;
+
+public interface IPoolManager : IMessenger<IPoolManager>
+{
+	public IReadOnlyDictionary<Type, IPool> Pools { get; }
+
+	public IPool<T> AddPool<T>(int maxCount = -1, bool fill = false) where T : new();
+
+	public IPool<T> AddPool<T>(Func<T> creator, int maxCount = -1, bool fill = false);
+
+	public bool RemovePool<T>();
+
+	public IPool<T> GetPool<T>(Type type = null);
+
+	public T Get<T>();
+
+	public T GetOrNew<T>() where T : new();
+
+	public bool Put<T>(T value);
+}
---
+++ Atlas.ECS/Core/Collections/Pool/PoolManager.cs
@@ -0,0 +1,70 @@
+﻿using Atlas.Core.Extensions;
+using Atlas.Core.Messages;
+using System;
+using System.Collections.Generic;
+
+namespace Atlas.Core.Collections.Pool;
+
+public sealed class PoolManager : Messenger<IPoolManager>, IPoolManager
+{
+	public static readonly IPoolManager Instance = new PoolManager();
+
+	private readonly Dictionary<Type, uint> references = new();
+	private readonly Dictionary<Type, IPool> pools = new();
+
+	private PoolManager() { } // PoolManager can only have one instance.
+
+	public override void Dispose() { } // PoolManager can't be disposed.
+
+	#region Pools
+	#region Get
+	public IReadOnlyDictionary<Type, IPool> Pools => pools;
+
+	public IPool<T> GetPool<T>(Type type = null) => pools.TryGetValue(type ?? typeof(T), out IPool<T> pool) ? pool : null;
+
+	public bool ContainsPool<T>() => GetPool<T>() != null;
+	#endregion
+
+	#region Add
+	public IPool<T> AddPool<T>(int maxCount = -1, bool fill = false) where T : new() => AddPool(() => new T(), maxCount, fill);
+
+	public IPool<T> AddPool<T>(Func<T> constructor, int maxCount = -1, bool fill = false)
+	{
+		var type = typeof(T);
+		if(!pools.TryGetValue(type, out IPool<T> pool))
+		{
+			pool = new Pool<T>(constructor, maxCount, fill);
+			pools.Add(type, pool);
+			references.Add(type, 0);
+		}
+		++references[type];
+		return pool;
+	}
+	#endregion
+
+	#region Remove
+	public bool RemovePool<T>()
+	{
+		var type = typeof(T);
+		if(pools.TryGetValue(type, out IPool<T> pool))
+		{
+			if(--references[type] <= 0)
+			{
+				pool.Dispose();
+				pools.Remove(type);
+				references.Remove(type);
+			}
+		}
+		return !pools.ContainsKey(type);
+	}
+	#endregion
+	#endregion
+
+	#region Instances
+	public T Get<T>() => pools.TryGetValue(typeof(T), out IPool<T> pool) ? pool.Get() : default;
+
+	public T GetOrNew<T>() where T : new() => pools.TryGetValue(typeof(T), out IPool<T> pool) ? pool.Get() : new T();
+
+	public bool Put<T>(T value) => GetPool<T>(value.GetType())?.Put(value) ?? false;
+	#endregion
+}
